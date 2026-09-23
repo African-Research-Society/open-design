02d174bf3ce7324471bc862b7e8638f610547f64
wheedie <waheed@wizmed.co.za>
2026-09-24 00:09:12 +0200

    Brand ARS design workspace as Kora Design

--- apps/web/src/branding.ts
+++ apps/web/src/branding.ts
@@ -1,5 +1,5 @@
 export const ARS_BRANDED = true;
-export const ARS_PRODUCT_NAME = 'ARS Design';
+export const ARS_PRODUCT_NAME = 'Kora Design';
 export const ARS_ORGANIZATION_NAME = 'African Research Society';
 export const ARS_HOME_URL = 'https://africanresearchsociety.org';
 export const ARS_PARENT_ORIGINS = [
